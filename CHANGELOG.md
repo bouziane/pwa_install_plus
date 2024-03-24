f8d69e0904ea0e5e3ecb1e766f756e26ab3bf6d0
bouziane <bo.hamzi@gmail.com>
2024-03-24 01:34:40 +0100

    commit init

---
+++ CHANGELOG.md
@@ -0,0 +1,3 @@
+## 0.0.1
+
+- Updated js package using pwa_install project for improved performance and enhanced functionality in our progressive web application?
---
+++ test/pwa_install_plus_test.dart
@@ -0,0 +1,8 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:pwa_install_plus/js_stub.dart';
+import 'package:pwa_install_plus/pwa_install_plus.dart';
+
+@JS('self')
+external dynamic currentWindow;
+
+void main() {}
