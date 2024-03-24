fab8145879124e0c192edd7954db9bca471d76db
bouziane <bo.hamzi@gmail.com>
2024-03-24 01:39:36 +0100

    Add license

--- test/pwa_install_plus_test.dart
+++ test/pwa_install_plus_test.dart
@@ -1,6 +1,4 @@
-import 'package:flutter_test/flutter_test.dart';
 import 'package:pwa_install_plus/js_stub.dart';
-import 'package:pwa_install_plus/pwa_install_plus.dart';
 
 @JS('self')
 external dynamic currentWindow;
